74c99082e5d827793a76bb5251819e7908e77ab9
Abdelkader Amar <abdelkader.amar@gmail.com>
2018-04-02 17:50:47 +0200

    Adding xml2fix generator

---
+++ tools/test_generator_xml2fix.cxx
@@ -0,0 +1,1 @@
+#include "test_generator_xml2fix.hxx"
---
+++ tools/test_generator_xml2fix.hxx
@@ -0,0 +1,16 @@
+#ifndef TEST_GENERATOR_HXX
+#define TEST_GENERATOR_HXX
+
+#include "test_generator.hxx"
+
+class test_generator_xml2fix : public test_generator
+{
+public:
+
+protected:
+
+private:
+};
+
+
+#endif /* end of include guard: TEST_GENERATOR_HXX */
